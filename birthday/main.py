835c077a1968fa5e642d15eb82dea12d8661555d
OwlFromDiscord <89514768+OwlFromDiscord@users.noreply.github.com>
2024-06-27 12:46:45 -0500

    made formatting on print a bit better

--- birthday/main.py
+++ birthday/main.py
@@ -7,4 +7,4 @@ days_away = next_birthday - today
 if next_birthday == today:
     print(birthday_messages.random_message)
 else:
-    print("My birthday is " + str(days_away) + " away!")
+    print(f'My birthday is {days_away} days away!')
