45d6d8c0649bde0c094204c2bc0d03251d5ae5b4
OwlFromDiscord <89514768+OwlFromDiscord@users.noreply.github.com>
2024-06-27 12:15:32 -0500

    Fixed datetime.today() to datetime.date.today(), fixed date to reflect current year. Fixed string concatenation.

--- birthday/main.py
+++ birthday/main.py
@@ -1,10 +1,10 @@
 import datetime, birthday_messages
 
-today = datetime.today()
-next_birthday = datetime.date(2001, 12, 10)
+today = datetime.date.today()
+next_birthday = datetime.date(2024, 12, 10)
 days_away = next_birthday - today
 
 if next_birthday == today:
     print(birthday_messages.random_message)
 else:
-    print("My birthday is " + days_away + " days away!")
+    print("My birthday is " + str(days_away) + " away!")
